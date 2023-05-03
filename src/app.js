a6e871afe470703386c007973df4292942bb03f4
Daniel Rodriguez <127802925+danielrodc@users.noreply.github.com>
2023-05-03 01:37:22 +0000

    my escuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["my", "A", "The"];
+  let subject = ["parrot", "dog", "cat", "fish"];
+  let action = ["took my", "pissed on", "ate mine"];
+  let object = ["homework", "baby", "boyfriends"];
+  let where = ["on my desk", "inside my car"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let objectIndex = Math.floor(Math.random() * object.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    object[objectIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
